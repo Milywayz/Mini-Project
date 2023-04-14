f41379428d1d5d5518ed748736a166e486ebe394
Milywayz <jtjenks@gmail.com>
2023-04-14 13:45:06 -0500

    Added a for loop in JS

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ fetch("https://www.loc.gov/search/?q=baseball&fo=json")
 console.log(data)
 
 })
+
+for (let i = 0; i < data.results.length; i++) {
+       
+        
+    let titleEl = document.createElement("p")
+    titleEl.textContent = data.results[i].title;
+
+    document.body.appendChild(titleEl);
+    
+    
+    let urlEl = document.createElement("a");
+    urlEl.textContent = data.results[i].url;
+    urlEl.href = data.results[i].url;
+
+    document.body.appendChild(titleEl);
+    document.body.appendChild(urlEl);
+    
+}
